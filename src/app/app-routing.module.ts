7cf264d6ec7564869e266f7975c67a91309cbfde
Esperanza Najera <esperanza.nave@gmail.com>
2022-11-07 11:38:23 -0700

    design views for features

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'regiter', component: RegisterComponent},
+    {path: '', component: DashboardComponent},
+    {path: '**', redirectTo:''}
+]
+
+@NgModule({
+
+    imports:[
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+
+})
+
+export class AppRoutingModule{}
