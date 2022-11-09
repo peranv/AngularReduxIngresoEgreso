da44f737c62cbd75be5d215ca35af608902bd6ed
Esperanza Najera <esperanza.nave@gmail.com>
2022-11-08 21:31:38 -0700

    design completed and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { dashboardRoutes } from './dashboard/dashboar.routes';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
     {path: 'login', component: LoginComponent},
-    {path: 'regiter', component: RegisterComponent},
-    {path: '', component: DashboardComponent},
+    {path: 'register', component: RegisterComponent},
+    {
+        path: '', 
+        component: DashboardComponent,
+        children: 
+            dashboardRoutes
+        
+    },
     {path: '**', redirectTo:''}
 ]
 
